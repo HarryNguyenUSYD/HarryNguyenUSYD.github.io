2248d7e3a2c0bbfb19b7441a4de881d49608aaa7
HarryNguyenUSYD <nguyenhoangquan2122@gmail.com>
2024-10-03 20:01:20 +1000

    Added favicon

--- Resources/JS/screenshot.js
+++ Resources/JS/screenshot.js
@@ -1,6 +1,8 @@
 function showFullScreenshot(src) {
     $("#display-screenshot > img").attr("src", src);
     $("#display-screenshot").css("display", "flex");
+    $("#display-screenshot > .background").css("display", "block");
+    $("#display-screenshot > .background").css("pointer-events", "none");
 
     setTimeout(function() {
         $("#display-screenshot > img").css("transform", "scale(1)");
@@ -8,8 +10,16 @@ function showFullScreenshot(src) {
 }
 
 function hideFullScreenshot() {
-    $("#display-screenshot > img").css("transform", "scale(0)");
-    $("#display-screenshot").css("display", "none");
+    $("#display-screenshot > .background").css("display", "none");
+    $("#display-screenshot > .background").css("pointer-events", "auto");
+
+    setTimeout(function() {
+        $("#display-screenshot > img").css("transform", "scale(0)");
+    }, 0);
+
+    setTimeout(function() {
+        $("#display-screenshot").css("display", "none");
+    }, 500);
 }
 
 function init() {
