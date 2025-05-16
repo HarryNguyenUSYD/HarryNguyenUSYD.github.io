abc888a5ac86039262efd92770131f871a48f2fd
HarryNguyenUSYD <126751195+HarryNguyenUSYD@users.noreply.github.com>
2025-05-16 15:40:51 +1000

    Writing blogs

--- src/global/mdx/mdxComponents.tsx
+++ src/global/mdx/mdxComponents.tsx
@@ -3,7 +3,8 @@ import ImageGallery from './ImageGallery/ImageGallery';
  
 export function useMDXComponents(components?: MDXComponents): MDXComponents {
     return {
-        h1: (props) => <h1 className="text-xl my-2 lg:text-4xl lg:my-5 font-bold" {...props} />,
+        h1: (props) => <h1 className="text-2xl my-2 lg:text-4xl lg:my-5 font-bold" {...props} />,
+        h2: (props) => <h2 className="text-xl my-2 lg:text-3xl lg:my-5 font-bold" {...props} />,
         p: (props) => <p className="text-base lg:text-2xl my-2" {...props} />,
         ul: (props) => <ul className="list-disc ml-6 my-4" {...props} />,
         li: (props) => <li className="mb-2" {...props} />,
